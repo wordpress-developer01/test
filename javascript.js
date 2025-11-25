e66c6168478d288dd0f5498c000b2792ff55bbac
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2025-11-25 12:33:52 +0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -105,3 +105,35 @@ function displayEncryptedMessage(encryptionMethod, userInput) {
   process.exit();
 }
 
+// Import packages
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const passport = require("passport");
+
+// App config
+app.set("trust proxy", 1);
+const PORT = process.env.PORT || 4001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+app.set("view engine", "ejs");
+// Import Passport config
+require("./config/passport");
+
+// Session Config
+
+// Passport Config
+
+// Routes
+app.use(require("./routes/index.routes"));
+
+app.get("/", (req, res) => {
+  const user = null || "Guest";
+  res.render("home", { user });
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: ${PORT}`);
+});
