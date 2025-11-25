a3aa2dafd636b0cb3dfb1e1da04ce7992fbfe28c
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2025-11-25 12:30:08 +0300

    Create php.php

---
+++ php.php
@@ -0,0 +1,24 @@
+<?php
+// Full name: John Smith
+// Fun fact: I can solve a Rubik's cube in under 30 seconds.
+
+// First initial: A
+echo "  A  \n";
+echo " A A \n";
+echo "AAAAA\n";
+echo "A   A\n";
+echo "A   A\n";
+echo "A   A\n";
+echo "A   A\n";
+
+echo "\n";
+
+// Second initial: B
+echo "BBBB \n";
+echo "B   B\n";
+echo "B   B\n";
+echo "BBBB \n";
+echo "B   B\n";
+echo "B   B\n";
+echo "BBBB \n";
+?>
