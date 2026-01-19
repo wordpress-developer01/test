705c440c3e6a45665a6237de4d31b29194497649
Muratbek Nassyrov <130246057+wordpress-developer01@users.noreply.github.com>
2026-01-19 17:15:17 +0300

    Add apple inventory details to main.c

--- main.c
+++ main.c
@@ -169,3 +169,19 @@ int main() {
   // output a line
   printf("Hello World!\n");
 }
+
+
+#include <stdio.h>
+
+int main() {
+  
+  int appleQuantity =23;
+  double applePrice = 1.49;
+  float appleReview = 82.5;
+  int appleReviewDisplay = appleReview;
+  const char appleLocation = 'F';
+
+// Put all your code above this and if you declare your variables using the given names and types there is no need to change any of the code below.
+printf("An apple costs: $%.2f, there are %d in inventory found in section: %c and your customers gave it an average review of %d%%!", applePrice, appleQuantity, appleLocation, appleReviewDisplay);
+
+}
